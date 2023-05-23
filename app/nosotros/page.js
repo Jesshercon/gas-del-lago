972cea66eaaaba32933cd289d51692ff2fd61460
jessica <jessicahercon@gmail.com>
2023-05-23 10:44:25 -0600

    about us section added to page.js

--- app/nosotros/page.js
+++ app/nosotros/page.js
@@ -1,9 +1,10 @@
+import AboutUsSection from "@/src/pages/aboutUs/AboutUsSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      NOSOTROS
+    <main className="flex min-h-screen flex-col items-center justify-between py-20">
+      <AboutUsSection />
     </main>
   );
 }
