0000cda56e45323252c932c7904a177f84d4b437
jessica <jessicahercon@gmail.com>
2023-05-23 10:43:27 -0600

    sponsorship section and footer added to main page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
+import Footer from "@/src/components/layout/Footer";
 import HomeHeroSection from "@/src/pages/home/HomeHeroSection";
 import ServicesSection from "@/src/pages/home/ServicesSection";
-import Image from "next/image";
+import SponsorshipSection from "@/src/pages/home/SponsorshipSection";
 
 export default function Home() {
   return (
     <main>
       <HomeHeroSection />
       <ServicesSection />
+      <SponsorshipSection />
+      <Footer />
     </main>
   );
 }
